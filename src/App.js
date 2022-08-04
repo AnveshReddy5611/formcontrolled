713dce6f2d719418552a833d4081b6aec577e3ea
Anvesh Reddy Akkati <anvesh.akkati@techmojo.in>
2022-08-04 12:23:42 +0530

    controlledform

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ControlledForm from './controlledform';
+
+function App() {
+  return (
+    <div className="App">
+     <ControlledForm/>
+    </div>
+  );
+}
+
+export default App;
